332d443eeac644326ec8db6e848c3ffe25603768
TMT <105305209+LtMtrunG@users.noreply.github.com>
2022-09-07 13:41:11 +0700

    Delete SCENEBUILDER directory

--- SCENEBUILDER/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.scenebuilder {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.controlsfx.controls;
-
-    opens com.example.scenebuilder to javafx.fxml;
-    exports com.example.scenebuilder;
-}
